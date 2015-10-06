43059b75535c5878df51dcb91372670bf2bdccf6
Ruel Nopal <radon1284@yahoo.com>
2015-10-06 14:22:03 +0800

    task logs a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  Time::DATE_FORMATS[:task_time] = "%H:%M %p"
 end
