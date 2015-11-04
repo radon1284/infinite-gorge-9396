2934ce5bde3298653b31b72b37ad97d0e5de870c
Ruel Nopal <radon1284@yahoo.com>
2015-11-04 16:27:20 +0800

    Working with the Time

--- app/controllers/task_logs_controller.rb
+++ app/controllers/task_logs_controller.rb
@@ -14,18 +14,6 @@ class TaskLogsController < ApplicationController
 
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
 
-    # @total_hrs = current_user.task_logs.sum(:total_hrs)
-    # @hr_min_value = @total_hrs.to_s.split(".").map { |s| s.to_i }
-    # @result_hr = @hr_min_value[0].to_s + ":"
-    # @result_min = ((@hr_min_value[1]*60)/100).to_s + " Hrs."
-    # @hr_min = @result_hr + @result_min
-
-#     @hrs_by_day = current_user.task_logs.sum(:total_hrs, 
-#                          :group => 'DATE(created_at)', 
-#                          :conditions => ['DATE(created_at) > ?', 31.days.ago])
-# (0..30).collect { |d| @hrs_by_day[d.days.ago.to_date.to_s] || 0 }.reverse
-
-
 
   end
 
--- app/models/task_log.rb
+++ app/models/task_log.rb
@@ -5,16 +5,20 @@ class TaskLog < ActiveRecord::Base
 	has_many :client
 	accepts_nested_attributes_for :client, :update_only => true
 
-	# before_save :sum_of_hrs
+	before_save :sum_of_hrs
+	before_save :total_time
 
 	def sum_of_hrs
-	  sum_value = (self.ending_time - self.starting_time)/3600
-	  hrs_value = "%.2f" % ((sum_value*60)/60)
-	  hr_min_value = hrs_value.to_s.split(".").map { |s| s.to_i }
-	  result_hr = hr_min_value[0].to_s + ":"
-	  result_min = ((hr_min_value[1]*60)/100).to_s + " Hrs."
-	  hr_min = result_hr + result_min
+		sum_value = (self.ending_time - self.starting_time)/3600
+		hrs_value = "%.2f" % ((sum_value*60)/60)
+		hr_min_value = hrs_value.to_s.split(".").map { |s| s.to_i }
+		result_hr = hr_min_value[0].to_s + ":"
+		result_min = ((hr_min_value[1]*60)/100).to_s + " Hrs."
+		hr_min = result_hr + result_min
 	end
-	private
+
+	def total_time
+		result_time = self.assign_attributes(total_hrs: sum_of_hrs)
+	end
 
 end
