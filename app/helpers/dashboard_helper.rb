564db41453445dc3b999961071a521c1d347feea
Ruel Nopal <radon1284@yahoo.com>
2015-11-30 20:34:41 +0800

    testing total hrs

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,7 +1,33 @@
 module DashboardHelper
 
-	def all_hrs(client)
-		client.hrs_this_month
+def my_helper
+    total_hrs_by_date.each do |client|
+      # content_tag(:tr)
+      # content_tag(:td, :data => {:title => "Name" }) + client.full_name
+      # content_tag(:td, :data => {:title => "Business Name" }) client.business_name
+      # content_tag(:td, :data => {:title => "Total Time" })
+      # c = ("%.2f" % client.hrs_this_month).to_s.split(".").map { |s| s.to_i }
+      # a = c[0].to_s + ":" + ((c[1]*60)/100).to_s + " Hrs."
+    end
+  end
+def mhsn
+	arr = ["a", "b", "c"]
+		content_tag(:tr) do
+		  arr.each do |item|
+		  concat content_tag(:td, item)
+		end
 	end
-  	
 end
+end
+
+# <tr>
+#   <td data-title="Name"><%= client.full_name %></td>
+#   <td data-title="Business Name"><%= client.business_name %></td>
+#   <td data-title="Month">
+#   <%= 
+#      c = ("%.2f" % client.hrs_this_month).to_s.split(".").map { |s| s.to_i }
+#      a = c[0].to_s + ":" + ((c[1]*60)/100).to_s + " Hrs."
+ 
+#   %></td>
+# </tr>
+
